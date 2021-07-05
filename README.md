c508265b0cc612c9bd29c1c8e9eb89f91fed77fd
calvin9150 <wonbae9150@gmail.com>
2021-07-05 18:15:47 +0900

    project first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# react-wblog
---
+++ back/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const db = require("./models");
+
+const postRouter = require("./routes/post");
+
+// dotenv.config();
+
+// db.sequelize.sync();
+
+const app = express();
+
+app.use("/post", postRouter);
+
+app.listen(3005, () => {
+  console.log("백엔드 서버 가동");
+});
