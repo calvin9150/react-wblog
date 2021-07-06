11b1e53e7786d74a69e1c380119ed222557f0d51
calvin9150 <wonbae9150@gmail.com>
2021-07-07 02:04:40 +0900

    Add posts route

---
+++ server/routes/posts.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { Post, User, Image, Comment } = require("../models");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      limit: 10,
+      order: [
+        ["createdAt", "DESC"],
+        [Comment, "createdAt", "DESC"],
+      ],
+
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+        {
+          model: Image,
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+            },
+          ],
+        },
+      ],
+    });
+    res.status(200).json(posts);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+module.exports = router;
