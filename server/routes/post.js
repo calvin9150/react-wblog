6f52fb215b37f76dd7a0f668d21f88a24affec2b
calvin9150 <wonbae9150@gmail.com>
2021-07-06 18:08:29 +0900

    Add insert post & comment

--- server/routes/post.js
+++ server/routes/post.js
@@ -2,8 +2,57 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  res.json("hi hi");
+const { Post, Comment, User, Image } = require("../models");
+const { isLoggedIn } = require("./middlewares");
+
+router.post("/", isLoggedIn, async (req, res, next) => {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      UserId: req.user.id,
+    });
+    const fullPost = await Post.findOne({
+      where: { id: post.id },
+      include: [
+        {
+          model: Image,
+        },
+        {
+          model: Comment,
+        },
+        {
+          model: User,
+        },
+      ],
+    });
+    res.status(201).json(fullPost);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.post("/:postId/comment", isLoggedIn, async (req, res, next) => {
+  try {
+    const post = await Post.findOne({
+      where: { id: req.params.post },
+    });
+    if (!post) {
+      return res.status(403).send("존재하지 않는 게시글입니다..");
+    }
+    const comment = await Comment.create({
+      content: req.body.content,
+      PostId: parseInt(req.params.postId, 10),
+      UserId: req.user.id,
+    });
+    await Post.create({
+      content: req.body.content,
+    });
+    res.status(201).json(comment);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 router.post("/", (req, res) => {});
