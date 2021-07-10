fb2df16f3de5e30f9ed83d8c8360b58d24d41da8
calvin9150 <wonbae9150@gmail.com>
2021-07-10 14:04:44 +0900

    Add type UseInput

--- client/hooks/useInput.ts
+++ client/hooks/useInput.ts
@@ -1,10 +1,23 @@
-import { useState, useCallback, ChangeEvent } from "react";
+import {
+  useState,
+  useCallback,
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+} from "react";
 
-const useInput = (initValue: any) => {
+type UseInput = [
+  string,
+  (e: ChangeEvent<HTMLInputElement>) => void,
+  Dispatch<SetStateAction<string>>
+];
+
+const useInput = (initValue: string): UseInput => {
   const [value, setValue] = useState(initValue);
   const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   }, []);
+
   return [value, handler, setValue];
 };
 
