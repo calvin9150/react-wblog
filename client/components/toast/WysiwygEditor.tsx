8d062594cd0742482321005b1c7223d7bf7bced3
calvin9150 <wonbae9150@gmail.com>
2021-07-13 03:03:49 +0900

    Add toast editor ui

---
+++ client/components/toast/WysiwygEditor.tsx
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+import dynamic from "next/dynamic";
+import * as React from "react";
+import { Editor as EditorType, EditorProps } from "@toast-ui/react-editor";
+import { TuiEditorWithForwardedProps } from "@/components/toast/Editor";
+
+const SubmitBtn = styled.button`
+  width: 100px;
+  height: 50px;
+  margin: 50px;
+  float: right;
+`;
+
+interface EditorPropsWithHandlers extends EditorProps {
+  onChange?(value: string): void;
+}
+
+const Editor = dynamic<TuiEditorWithForwardedProps>(() => import("./Editor"), {
+  ssr: false,
+});
+const EditorWithForwardedRef = React.forwardRef<
+  EditorType | undefined,
+  EditorPropsWithHandlers
+>((props, ref) => (
+  <Editor {...props} forwardedRef={ref as React.MutableRefObject<EditorType>} />
+));
+
+interface Props extends EditorProps {
+  onChange(value: string): void;
+
+  valueType?: "markdown" | "html";
+}
+
+const WysiwygEditor: React.FC<Props> = (props) => {
+  const [content, setContent] = React.useState<any>();
+  const {
+    initialValue,
+    previewStyle,
+    height,
+    initialEditType,
+    useCommandShortcut,
+  } = props;
+
+  const editorRef = React.useRef<EditorType>();
+  const handleChange = React.useCallback(() => {
+    if (!editorRef.current) {
+      return;
+    }
+
+    const instance = editorRef.current.getInstance();
+    const valueType = props.valueType || "markdown";
+
+    setContent(instance.getHtml());
+
+    props.onChange(
+      valueType === "markdown" ? instance.getMarkdown() : instance.getHtml()
+    );
+  }, [props, editorRef]);
+
+  const handleEditor = () => {
+    console.log(content);
+  };
+
+  return (
+    <>
+      <div>
+        <EditorWithForwardedRef
+          {...props}
+          initialValue={initialValue}
+          previewStyle={previewStyle || "vertical"}
+          height={height || "600px"}
+          initialEditType={initialEditType || "markdown"}
+          useCommandShortcut={useCommandShortcut || true}
+          ref={editorRef}
+          onChange={handleChange}
+        />
+      </div>
+      <SubmitBtn onClick={handleEditor}>제출</SubmitBtn>
+    </>
+  );
+};
+
+export default WysiwygEditor;
--- client/pages/edit/index.tsx
+++ client/pages/edit/index.tsx
@@ -1,12 +1,31 @@
 import Head from "next/head";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+
+import { loadUser } from "@/actions/user";
+import WysiwygEditor from "@/components/toast/WysiwygEditor";
+
+const Container = styled.div`
+  width: 80%;
+  margin: 100px auto;
+`;
 
 export default function Signup() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUser());
+  });
   return (
     <>
       <Head>
         <title>Wblog 글쓰기</title>
       </Head>
       <h1> 글쓰기 </h1>
+      <Container>
+        <WysiwygEditor onChange={(value) => console.log(value)} />
+      </Container>
     </>
   );
 }
