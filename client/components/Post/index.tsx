671f5692359e380ca77033a0336276ab453389c8
calvin9150 <wonbae9150@gmail.com>
2021-07-17 01:32:57 +0900

    Add comment form

--- client/components/Post/index.tsx
+++ client/components/Post/index.tsx
@@ -1,7 +1,7 @@
 import { deletePost } from "@/actions/post";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import Link from "next/link";
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
 
@@ -23,8 +23,39 @@ const Title = styled.div`
 const Content = styled.div`
   width: 100%;
   min-height: 150px;
-  /* border: 1px solid black; */
   overflow: hidden;
+`;
+
+const Comment = styled.div`
+  width: 100%;
+  height: 200px;
+  margin: 50px 0;
+  border-top: 2px ridge rgba(135, 137, 137, 0.267);
+
+  p {
+    color: #8080807b;
+  }
+
+  textarea {
+    width: 100%;
+    height: 100px;
+    font-size: 17px;
+    box-sizing: border-box;
+    outline: none;
+    border: 1px ridge rgba(135, 137, 137, 0.267);
+  }
+
+  button {
+    height: 50px;
+    width: 100px;
+    float: right;
+    border: 1px ridge rgba(135, 137, 137, 0.267);
+    border-radius: 15px 15px;
+    background-color: #a5a5a528;
+    :hover {
+      background-color: #0044ff29;
+    }
+  }
 `;
 
 const Post: FC<AppProps> = ({ post }) => {
@@ -32,6 +63,8 @@ const Post: FC<AppProps> = ({ post }) => {
   const title = post.title;
   const content = post.content;
   const id = post.id;
+
+  const [count, setCount] = useState(0);
 
   const createContent = () => {
     return { __html: content };
@@ -48,7 +81,16 @@ const Post: FC<AppProps> = ({ post }) => {
           <Title>{title}</Title>
         </Link>
         <Content dangerouslySetInnerHTML={createContent()} />
+
         {/* <button onClick={deleteClick}>삭제</button> */}
+        <Comment>
+          <p>{count}/200</p>
+          <textarea
+            maxLength={200}
+            onChange={(e) => setCount(e.target.value.length)}
+          ></textarea>
+          <button>댓글 등록</button>
+        </Comment>
       </Layout>
     </>
   );
